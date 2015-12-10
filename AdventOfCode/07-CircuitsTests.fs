a47eba1a5bcce7ad1c07e6e6189ce609aab81f22
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-10 16:43:08 +0100

    added tests for simple inputs

--- AdventOfCode/07-CircuitsTests.fs
+++ AdventOfCode/07-CircuitsTests.fs
@@ -6,5 +6,10 @@ open Swensen.Unquote
 open Circuits
 
 [<Fact>]
-let worldIsSane() =
+let simpleInputs() =
     test <@ calculateAllSignalsFor "123 -> x" = [("x", 123us)] @>
+    test <@ calculateAllSignalsFor "NOT 1 -> x" = [("x", 65534us)] @>
+    test <@ calculateAllSignalsFor "1 OR 2 -> x" = [("x", 3us)] @>
+    test <@ calculateAllSignalsFor "6 AND 2 -> x" = [("x", 2us)] @>
+    test <@ calculateAllSignalsFor "1 LSHIFT 2 -> x" = [("x", 4us)] @>
+    test <@ calculateAllSignalsFor "8 RSHIFT 2 -> x" = [("x", 2us)] @>
