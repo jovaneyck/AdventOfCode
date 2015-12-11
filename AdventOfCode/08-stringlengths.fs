89f1a2a4ebb196c4e775a6c57c63336006227f0e
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-11 10:33:03 +0100

    finished day 7. YAY! On to day 8!

---
+++ AdventOfCode/08-stringlengths.fs
@@ -0,0 +1,16 @@
+﻿module StringLengths
+
+open Xunit
+open Swensen.Unquote
+
+type Line = | Line of string
+
+let stringLength (Line l) = l.Length
+
+[<Theory>]
+[<InlineData(@"""""", 2)>]
+[<InlineData(@"""abc""", 5)>]
+[<InlineData(@"""aaa\""aaa""", 10)>]
+[<InlineData(@"""\x27""", 6)>]
+let stringLengthOfASingleLine (input, expectedLength) =
+    test <@ stringLength (Line input) = expectedLength @>
