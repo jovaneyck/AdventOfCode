87d4e238415781dd88a54fc4e9e33b5c0e974cdc
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-14 08:08:45 +0100

    got a correct version but needs tail recursion

--- AdventOfCode/00-sandbox.fsx
+++ AdventOfCode/00-sandbox.fsx
@@ -1,1 +1,26 @@
-﻿
+﻿open System
+
+let rec hearSayChars chars = 
+    match chars with
+    | [] -> []
+    | x :: xs -> 
+        let equalToX = (x :: (xs |> List.takeWhile ((=) x)))
+        let tail = (xs |> List.skipWhile ((=) x) |> hearSayChars)
+        equalToX :: tail
+
+let hearSay (number : string) =
+    number.ToCharArray()
+    |> List.ofArray
+    |> hearSayChars
+    |> List.map (fun ident -> (ident |> List.length, ident |> List.head))
+    |> List.map (fun (cnt, c) -> cnt.ToString() + c.ToString())
+    |> List.reduce (+)
+
+let rec repeat f times input =
+    match times with
+    | 0 -> input
+    | n -> repeat f (n - 1) (f input)
+
+let repeatHearSay = repeat hearSay 40
+
+let result = repeatHearSay "1113122113"
