835412afb6bf5d5682b4390c23e38621807442dd
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-16 16:18:24 +0100

    day 12 complete. Yay!

--- AdventOfCode/12-JSON-numbers.fsx
+++ AdventOfCode/12-JSON-numbers.fsx
@@ -1,5 +1,4 @@
 ﻿#load "12-JsonNumbersInput.fs"
-
 open System.Text.RegularExpressions
 
 let x = 
@@ -8,3 +7,11 @@ let x =
     |> List.filter ((<>) "")
     |> List.map (fun nb -> System.Int32.Parse(nb))
     |> List.sum
+
+#r "./bin/Debug/FSharp.Data.dll"
+open FSharp.Data
+
+#load "12-JSON-numbers-without-red.fs"
+open JSONNumbersWithoutRed
+
+JsonNumbersInput.input |> parse |> allNonRedNumbers |> List.sum
--- AdventOfCode/Main.fs
+++ AdventOfCode/Main.fs
@@ -1,12 +1,9 @@
 ﻿module App
 
 open System
-open HearSay
 
 [<EntryPoint>]
 let main args = 
     printfn "Go!"
-    let result = repeatHearSay 50 "1113122113" |> String.length
-    printfn "The magic number: %d" result
     Console.ReadKey() |> ignore
     0
