0202e924a095e0832fe59a81bd0ff54289cc02bf
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-16 08:07:14 +0100

    finished day 11 passwords. yay!

---
+++ AdventOfCode/10-hearsay.fs
@@ -0,0 +1,38 @@
+﻿module HearSay
+
+open System
+
+let rec hearSayChars acc chars = 
+    match chars with
+    | [] -> acc
+    | x :: xs -> 
+        hearSayChars 
+            ((x :: (xs |> List.takeWhile ((=) x)))::acc)
+            (xs |> List.skipWhile ((=) x))
+
+let memoize f =
+    let cache = ref Map.empty
+    fun x ->
+        match (!cache).TryFind(x) with
+        | Some res -> res
+        | None ->
+                let res = f x
+                cache := (!cache).Add(x,res)
+                res
+
+let hearSay (number : string) =
+    number.ToCharArray()
+    |> List.ofArray
+    |> ((memoize hearSayChars) [])
+    |> List.rev
+    |> List.map (fun identicals -> (identicals |> List.length, identicals |> List.head))
+    |> List.map (fun (nb, char) -> nb.ToString() + char.ToString())
+    |> List.reduce (+)
+
+let rec repeat f times input =
+    printfn "Iteration %d - input has length %d" times (input |> String.length)
+    match times with
+    | 0 -> input
+    | n -> repeat f (n - 1) (f input)
+
+let repeatHearSay = repeat (memoize hearSay)
--- AdventOfCode/Main.fs
+++ AdventOfCode/Main.fs
@@ -1,20 +1,12 @@
 ﻿module App
 
 open System
-open StringLengths
-open StringlengthsInput
+open HearSay
 
 [<EntryPoint>]
 let main args = 
-    printf "Hello!"
-    realInput
-    |> List.ofSeq
-    |> calculateLenghtsFor
-//    |> Seq.iter (fun (Line l, ll, iml) -> (printfn "WUT? Line: %s \n stringlength: %d \n inmem: %d" l ll iml))
-    |> Seq.map (fun (l, string, inmem, encoded) -> (l, encoded - string))
-    |> Seq.map (fun (_, delta) -> delta)
-    |> Seq.sum
-    |> (printfn "Total delta: %d")
-//    |> (printf "Total difference between string and inmem length = %d")
+    printfn "Go!"
+    let result = repeatHearSay 50 "1113122113" |> String.length
+    printfn "The magic number: %d" result
     Console.ReadKey() |> ignore
     0
