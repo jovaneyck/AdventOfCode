5f7e3f87ab0843f1903d3a60e32894efca145eed
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-16 08:29:06 +0100

    finished 12 part 1. Now on to the hard part!

---
+++ AdventOfCode/12-JSON-numbers.fsx
@@ -0,0 +1,10 @@
+﻿#load "12-JsonNumbersInput.fs"
+
+open System.Text.RegularExpressions
+
+let x = 
+    Regex.Split(JsonNumbersInput.input, "[^\-0-9.]")
+    |> List.ofArray
+    |> List.filter ((<>) "")
+    |> List.map (fun nb -> System.Int32.Parse(nb))
+    |> List.sum
