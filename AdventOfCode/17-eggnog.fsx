80bfaa7a01bc9bec622e7e63e9987c8a9c4f12be
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-22 10:22:30 +0100

    day 17 part 1 complete

---
+++ AdventOfCode/17-eggnog.fsx
@@ -0,0 +1,42 @@
+﻿let rec possibleCombinations amount c = 
+    match c with
+    | [] -> [[]]
+    | h :: t  when h <= amount -> 
+        let withTail = possibleCombinations (amount - h) t |> List.map(fun el -> h :: el)
+        let withoutTail = possibleCombinations amount t
+        withTail @ withoutTail
+    | _ :: t -> possibleCombinations amount t
+
+let validCombinations amount containers =
+    let isValid combo = 
+        combo 
+        |> List.sum 
+        |> (=) amount
+    possibleCombinations amount containers
+    |> List.filter isValid
+
+let adventInput = @"50
+44
+11
+49
+42
+46
+18
+32
+26
+40
+21
+7
+18
+43
+10
+47
+36
+24
+22
+40"
+
+let parsedInput = adventInput.Split('\n') |> List.ofArray |> List.map(fun el -> el |> System.Int32.Parse)
+let exampleContainers = [20;15;10;5;5]
+
+validCombinations 150 parsedInput |> List.length
