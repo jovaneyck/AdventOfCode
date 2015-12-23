65ecdd693bea8febbb7aeaf3d33272be69116517
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-23 12:13:16 +0100

    added parsing logic for day 19

---
+++ AdventOfCode/19-RewriteDNA.fsx
@@ -0,0 +1,22 @@
+﻿type Token = char list
+type RewriteRule = {Src : Token; Dest : Token}
+
+let rulesInput = @"H => HO
+H => OH
+O => HH"
+
+let parseToken (token : string) = 
+    token.ToCharArray() 
+    |> List.ofArray
+
+let parseLine (l : string) =
+    let tokens = l.Split([|" => "|], System.StringSplitOptions.RemoveEmptyEntries)
+    { Src = tokens.[0] |> parseToken; Dest = tokens.[1] |> parseToken}
+
+let parse (input : string) =
+    input.Split('\n')
+    |> List.ofArray
+    |> List.map parseLine
+
+rulesInput
+|> parse
