cc9b6f4043c35a81aa776bd7fc569ce32d1be6e8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2015-12-29 20:09:46 +0100

    got basic algorithm for day 19 down

--- AdventOfCode/19-RewriteDNA.fsx
+++ AdventOfCode/19-RewriteDNA.fsx
@@ -4,7 +4,7 @@ type RewriteRule = {Src : Token; Dest : Token}
 let rulesInput = @"H => HO
 H => OH
 O => HH"
-let textInput = "HOH"
+let textInput = "HOHOHO"
 
 let parseToken (token : string) = 
     token.ToCharArray() 
@@ -19,17 +19,34 @@ let parse (input : string) =
     |> List.ofArray
     |> List.map parseLine
 
-let allRewrites text rule = [text; text]
+let rec (|StartsWith|_|) (pattern, text) = 
+    match (pattern, text) with
+    | ([], t) -> Some (StartsWith t)
+    | (ph :: _, th :: _) when ph <> th -> None
+    | (ph :: pt, th :: tt) when ph = th ->
+        match (pt, tt) with
+        | StartsWith remainder -> Some remainder
+        | _ -> None
+    | what -> failwith (sprintf "Was not expecting %A in this context!" what)
+
+let rec allRewrites (text : char list) ({Src = s; Dest = d} as rule : RewriteRule) = 
+    match (s, text) with
+    | (_, []) -> [[]] 
+    | StartsWith remainder -> (d @ remainder) :: ((allRewrites remainder rule) |> List.map(fun l -> s @ l))
+    | (_, h :: t)  -> (allRewrites t rule) |> List.map (fun l -> h :: l)
 
 let parseText (text : string) = text.ToCharArray() |> List.ofArray
 
-let toString (chars : char list) = chars |> Array.ofList |> System.String
+let toString (chars : char list) : string = chars |> System.String.Concat
 
 let rewritesFor rawRules rawText =
+    let parsedInput = rawText |> parseText
     rawRules
     |> parse
-    |> List.collect (allRewrites (rawText |> parseText)) 
+    |> List.collect (allRewrites parsedInput) 
+    |> List.except [parsedInput]
     |> List.map toString
     |> List.distinct
 
-rewritesFor rulesInput textInput
+let rewrites = rewritesFor rulesInput textInput
+let nbRewrites = rewrites |> List.length
